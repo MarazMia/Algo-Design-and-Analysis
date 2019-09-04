dcdc869af59e105164ea4895bd9474eee2841b5d
Maraz Mia <43583025+MarazMia@users.noreply.github.com>
2019-09-05 02:12:22 +0600

    Update editDistance.cpp

--- editDistance.cpp
+++ editDistance.cpp
@@ -68,7 +68,7 @@ void fprint(int m,int n)
         }
         else if(b[m][n]=='l')
         {
-            cout<<"insert "<<y[n]<<" at position "<<n-1<<endl;
+            cout<<"insert "<<y[n-1]<<" at position "<<n-1<<endl;
             fprint(m,n-1);
         }
     }
